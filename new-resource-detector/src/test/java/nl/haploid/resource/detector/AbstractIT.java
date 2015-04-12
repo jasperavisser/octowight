995a675c2301dd5a1ea31625304af906173688dc
Jasper A. Visser <jasper.a.visser@gmail.com>
2015-04-12 21:58:12 +0200

    Remove unneeded dependencies

--- new-resource-detector/src/test/java/nl/haploid/resource/detector/AbstractIT.java
+++ new-resource-detector/src/test/java/nl/haploid/resource/detector/AbstractIT.java
@@ -1,11 +1,11 @@
 package nl.haploid.resource.detector;
 
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @ContextConfiguration(
-		classes = {TestConfiguration.class, KafkaConfiguration.class, PostgresConfiguration.class},
+		classes = {TestConfiguration.class, KafkaConfiguration.class},
 		loader = AnnotationConfigContextLoader.class)
-public abstract class AbstractIT extends AbstractTransactionalJUnit4SpringContextTests {
+public abstract class AbstractIT extends AbstractJUnit4SpringContextTests {
 }
