034af0967f7823db9a4547c19925b47a8321f27b
Jasper A. Visser <jasper.a.visser@gmail.com>
2015-04-13 22:05:47 +0200

    Add POC new resource detector

---
+++ new-resource-detector-poc/src/main/java/nl/haploid/resource/detector/App.java
@@ -0,0 +1,10 @@
+package nl.haploid.resource.detector;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(App.class);
+	}
+}
