7ee1db6866a262fddcdcc8e692a3a047e74caffc
Jasper A. Visser <jasper.a.visser@gmail.com>
2015-04-21 17:57:05 +0200

    Use IP of eth0 instead of localhost

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -5,5 +5,5 @@ if [[ -n "$(which boot2docker)" ]]; then
     $(boot2docker shellinit)
     export DOCKER_HOST_IP=$(boot2docker ip)
 else
-    export DOCKER_HOST_IP=127.0.0.1
+    export DOCKER_HOST_IP=$(ip -f inet -o addr show eth0 | cut -d\ -f 7 | cut -d/ -f 1)
 fi
