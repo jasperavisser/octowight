cbc9b81816617117d229a0fda58a718c9bb1526a
Jakub Szelagowski <j.szelagowski@portavita.eu>
2015-06-26 10:39:06 +0200

    Improve bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Configure docker host
-if [[ -n "$(which boot2docker)" ]]; then
+if which boot2docker 2>1 >/dev/null; then
     $(boot2docker shellinit)
     export DOCKER_HOST_IP=$(boot2docker ip)
 else
-    export DOCKER_HOST_IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+    export DOCKER_HOST_IP=$(hostname -I | tr -s " " "\n" | head -n 1)
 fi
+
+echo "Docker host is set to $DOCKER_HOST_IP"
