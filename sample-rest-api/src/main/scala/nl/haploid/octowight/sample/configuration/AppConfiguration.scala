fc5ae40cd26a96377285f8d51d1246a0447bd258
Jasper A. Visser <jasper.a.visser@gmail.com>
2015-06-26 17:07:28 +0200

    Allow all modules to be configured with local.properties

--- sample-rest-api/src/main/scala/nl/haploid/octowight/sample/configuration/AppConfiguration.scala
+++ sample-rest-api/src/main/scala/nl/haploid/octowight/sample/configuration/AppConfiguration.scala
@@ -1,6 +1,6 @@
 package nl.haploid.octowight.sample.configuration
 
-import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.{Configuration, PropertySource, Bean}
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
 
 object AppConfiguration {
@@ -8,4 +8,6 @@ object AppConfiguration {
   @Bean def propertyPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer
 }
 
+@PropertySource(value = Array("file:./local.properties"), ignoreResourceNotFound = true)
+@Configuration
 class AppConfiguration
