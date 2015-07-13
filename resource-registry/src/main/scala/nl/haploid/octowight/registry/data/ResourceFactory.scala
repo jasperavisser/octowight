d88cdb6dd1fa222c5a84ee3131cc6b5b08da6369
Jasper Visser <j.visser@portavita.eu>
2015-07-13 17:58:18 +0200

    Remove old resource class

--- resource-registry/src/main/scala/nl/haploid/octowight/registry/data/ResourceFactory.scala
+++
@@ -1,6 +0,0 @@
-package nl.haploid.octowight.registry.data
-
-trait ResourceFactory[R <: Resource[_]] {
-
-  def fromResourceRoot(resourceRoot: ResourceRoot): Option[R]
-}
--- resource-registry/src/main/scala/nl/haploid/octowight/registry/repository/ResourceModelDmo.scala
+++ resource-registry/src/main/scala/nl/haploid/octowight/registry/repository/ResourceModelDmo.scala
@@ -2,18 +2,10 @@ package nl.haploid.octowight.registry.repository
 
 import java.lang
 
-import nl.haploid.octowight.registry.data.Resource
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 import scala.annotation.meta.field
-
-object ResourceModelDmo {
-
-  def apply(resource: Resource[_], body: String) = {
-    new ResourceModelDmo(id = new ResourceModelDmoId(resource.id, resource.collection), body = body, version = resource.version)
-  }
-}
 
 @Document(collection = "resourceModel")
 case class ResourceModelDmo
